ac975350922c654aae98268dfd27bb4f95329f61
నాగరాజు <36604320+beautifulnature@users.noreply.github.com>
2021-06-29 10:44:57 +0000

    parallelogram

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-# Create the turtle
-# Go 150 steps forward
-# Turn 120 degrees left
-# Go 150 steps forward
-# Turn 120 degrees left
-# Go 150 steps forward
-# Turn 120 degrees left
-# Turn 60 degrees right
-# Draw a circle with radius 87
-
 from turtle import *
 shape('turtle')
 forward(150)
+left(60)
+forward(100)
 left(120)
 forward(150)
-left(120)
-forward(150)
-left(120)
-right(60)
-circle(87)
-
+left(60)
+forward(100)
+left(60)
+hideturtle()
